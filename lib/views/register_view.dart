635f4cfe920173da10f68bd42e66b8b9c9787fa6
max <m.levytskiy@gmail.com>
2022-04-06 11:42:40 +0300

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -3,6 +3,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 import '../firebase_options.dart';
 import 'dart:developer' as devtools show log;
@@ -76,18 +78,22 @@ class _RegisterViewState extends State<RegisterView> {
                             password: password
                         );
                         devtools.log(userCredential.toString());
+                        final user = FirebaseAuth.instance.currentUser;
+                        await user?.sendEmailVerification();
+                        Navigator.of(context).pushNamed(verifyEmailRoute);
                       } on FirebaseAuthException catch (e) {
                         if (e.code == 'weak-password') {
-                          devtools.log("Weak password");
+                          await showErrorDialog(context, "Weak password");
                         } else if (e.code == 'email-already-in-use') {
-                          devtools.log("Email already in use");
+                          await showErrorDialog(context, "Email already in use");
                         } else if (e.code == 'invalid-email') {
-                          devtools.log("Invalid email");
+                          await showErrorDialog(context, 'invalid-email');
                         } else {
-                          devtools.log("firebase auth exception happened ${e.code}");
+                          await showErrorDialog(context, "firebase auth exception happened ${e.code}");
                         }
                       } catch (e) {
                         devtools.log("Something bad happened $e");
+                        await showErrorDialog(context, "Something bad happened $e");
                       }
                     },
                         child: const Text('Register'))
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -42,7 +42,8 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
             ],
         ),
         body: Column(children: [
-          const Text('Please verify your email address:'),
+          const Text("We've sent you an email verification. Please open it to verify your account."),
+          const Text("If you haven't received a verification email yet, press the button below"),
           TextButton(
               onPressed: () async {
                 final user = FirebaseAuth.instance.currentUser;
@@ -53,7 +54,15 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
                   devtools.log('User is null');
                 }
               },
-              child: const Text('Send email verification'))
+              child: const Text('Send email verification'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
+            },
+            child: const Text('Restart'),
+          )
         ]));
   }
 }
