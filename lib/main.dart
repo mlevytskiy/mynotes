10ce96d00b54b22fe1f29c55c54a2058d597bf79
max <m.levytskiy@gmail.com>
2022-04-10 14:01:10 +0300

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/views/notes/new_note_view.dart';
 
 import 'constants/routes.dart';
 import 'views/login_view.dart';
-import 'views/notes_view.dart';
+import 'views/notes/notes_view.dart';
 import 'views/register_view.dart';
 import 'dart:developer' as devtools show log;
 
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       }
     );
   }
